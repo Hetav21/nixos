b7a17f7a4f415e121f9daaac44f86affd362234b
Hetav21 <shahhetav2106@gmail.com>
2024-12-05 14:12:09 +0530

    64 current  2024-12-05 14:12:02  25.05.20241203.55d15ad  6.11.5-zen1                          *

--- hosts/nixbook/home.nix
+++ hosts/nixbook/home.nix
@@ -188,7 +188,7 @@ in {
                                       case_sensitive: false # case-sensitive completions
                                       quick: true    # set to false to prevent auto-selecting completions
                                       partial: true    # set to false to prevent partial filling of the prompt
-                                      algorithm: "fuzzy"    # prefix or fuzzy
+                                      algorithm: "prefix"    # prefix or fuzzy
                                       external: {
                                       # set to false to prevent nushell looking into $env.PATH to find more suggestions
                                           enable: true
