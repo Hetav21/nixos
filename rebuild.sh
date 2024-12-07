71e8c409d6cd155bcd87fcf99d7b063e11eca5d6
Hetav21 <shahhetav2106@gmail.com>
2024-12-07 13:49:50 +0530

    81 current  2024-12-07 13:49:43  25.05.20241203.55d15ad  6.11.5-zen1                          *

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,7 @@
 set -e
 pushd /etc/nixos/
 alejandra . &>/dev/null
+cp flake.lock flake.lock.bak
 git diff -U0 flake.lock
 echo "NixOS Rebuilding..."
 sudo nixos-rebuild switch &>nixos-switch.log || (
