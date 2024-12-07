c9e5dded8ab17ca3768e739438215cefd4e244e0
Hetav21 <shahhetav2106@gmail.com>
2024-12-07 13:12:13 +0530

    Changing rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 set -e
 pushd /etc/nixos/
 alejandra . &>/dev/null
-git diff -U0 *.nix
+git diff -U0 flake.lock
 echo "NixOS Rebuilding..."
 sudo nixos-rebuild switch &>nixos-switch.log || (
  cat nixos-switch.log | grep --color error && false)
