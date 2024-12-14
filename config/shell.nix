e231e542393a9e82bb251406a69fdfb46fa423df
Hetav21 <shahhetav2106@gmail.com>
2024-12-14 12:58:57 +0530

    146 current  2024-12-14 12:58:52  25.05.20241211.5d67ea6  6.12.2-zen1                          *

--- config/shell.nix
+++ config/shell.nix
@@ -58,77 +58,27 @@
         paste = "wl-paste";
       };
       extraConfig = ''
-               let fish_completer = {|spans|
-                   fish --command $'complete "--do-complete=($spans | str join " ")"'
-                   | from tsv --flexible --noheaders --no-infer
-                   | rename value description
-               }
+          def lsfind [] {
+                             ll "$1" | grep "$2"
+          }
 
-               let carapace_completer = {|spans: list<string>|
-                   carapace $spans.0 nushell ...$spans
-                   | from json
-                   | if ($in | default [] | where value =~ '^-.*ERR$' | is-empty) { $in } else { null }
-               }
+          def warp [] {
+                     sudo systemctl "$1" warp-svc
+          }
 
-        let zoxide_completer = {|spans|
-        	   $spans | skip 1 | zoxide query -l ...$in | lines | where {|x| $x != $env.PWD}
-        }
+                 # Starship
+                          use ~/.cache/starship/init.nu
 
-               # This completer will use carapace by default
-               let external_completer = {|spans|
-                   let expanded_alias = scope aliases
-                   | where name == $spans.0
-                   | get -i 0.expansion
+                  # NPM global packages
+        $env.PATH = ($env.PATH |
+        split row (char esep) |
+        prepend /home/hetav/.npm-global/bin |
+        append /home/hetav/.npm-global/bin
+        )
 
-                   let spans = if $expanded_alias != null {
-                       $spans
-                       | skip 1
-                       | prepend ($expanded_alias | split row ' ' | take 1)
-                   } else {
-                       $spans
-                   }
-
-                   match $spans.0 {
-                       # carapace completions are incorrect for nu
-                       nu => $fish_completer
-                       # fish completes commits and branch names in a nicer way
-                       git => $fish_completer
-                       # carapace doesn't have completions for asdf
-                       asdf => $fish_completer
-                       # use zoxide completions for zoxide commands
-                       __zoxide_z | __zoxide_zi => $zoxide_completer
-                       _ => $carapace_completer
-                   } | do $in $spans
-               }
-
-               $env.config = {
-                   # ...
-                   completions: {
-                       external: {
-                           enable: true
-                           completer: $external_completer
-                       }
-                   }
-                   # ...
-               }
-
-                                       def lsfind [] {
-                                                          ll "$1" | grep "$2"
-                                       }
-
-                                       def warp [] {
-                                                  sudo systemctl "$1" warp-svc
-                                       }
-
-                                              # Starship
-                                                       use ~/.cache/starship/init.nu
-
-                                               # NPM global packages
-                                               $env.PATH = ($env.PATH | append "home/hetav/.npm-global/bin")
-
-                                               # Command Run
-                                               date
-                                               microfetch
+                  # Command Run
+                  date
+                  microfetch
       '';
     };
     fish.enable = true;
