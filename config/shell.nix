f78975be7d904b925f1042e134c465f44f635870
Hetav21 <shahhetav2106@gmail.com>
2024-12-14 14:18:59 +0530

    170 current  2024-12-14 14:18:52  25.05.20241211.5d67ea6  6.12.2-zen1                          *

--- config/shell.nix
+++ config/shell.nix
@@ -69,7 +69,7 @@
                }
 
         let fish_completer = {|spans|
-         fish --command complete --do-complete=$"($spans | str join " ")"
+         fish --command complete --do-complete=($spans | str join " ")
          | from tsv --flexible --noheaders --no-infer
          | rename value description
         }
