cd7adbfddb15b6347c233a8fd86e980a2d12badc
Hetav21 <shahhetav2106@gmail.com>
2025-01-21 22:43:05 +0530

    280 current  2025-01-21 22:42:56  25.05.20250114.eb62e6a  6.12.9-cachyos                          *

--- hosts/nixbook/home.nix
+++ hosts/nixbook/home.nix
@@ -100,17 +100,17 @@ in {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
+    #  gtk3.extraConfig = {
+    #    gtk-application-prefer-dark-theme = 1;
+    #  };
+    #  gtk4.extraConfig = {
+    #    gtk-application-prefer-dark-theme = 1;
+    #  };
   };
   qt = {
     enable = true;
     style.name = "adwaita-dark";
-    platformTheme.name = "gtk3";
+    #  platformTheme.name = "gtk3";
   };
 
   services.hypridle = {
