000ac159b9d9d05fea010139ed099323eceb5c26
Hetav21 <shahhetav2106@gmail.com>
2025-02-16 17:39:56 +0530

    360 current  2025-02-16 17:39:54  25.05.20250210.64e75cd  6.13.1-zen1                          *

--- common/units/systemd.nix
+++ common/units/systemd.nix
@@ -8,24 +8,24 @@
 }: let
 in {
   systemd = {
-    targets = {
-      sleep = {
-        enable = true;
-        unitConfig.DefaultDependencies = "yes";
-      };
-      suspend = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-      hibernate = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-      "hybrid-sleep" = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-    };
+    #    targets = {
+    #      sleep = {
+    #        enable = true;
+    #        unitConfig.DefaultDependencies = "yes";
+    #      };
+    #      suspend = {
+    #        enable = false;
+    #        unitConfig.DefaultDependencies = "no";
+    #      };
+    #      hibernate = {
+    #        enable = false;
+    #        unitConfig.DefaultDependencies = "no";
+    #      };
+    #      "hybrid-sleep" = {
+    #        enable = false;
+    #        unitConfig.DefaultDependencies = "no";
+    #      };
+    #    };
 
     services = {
       ###  To disable a systemd service:
