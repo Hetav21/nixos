b7f727453b7e067012979324e899afbd65da7291
Hetav21 <shahhetav2106@gmail.com>
2025-05-05 22:56:22 +0530

    123 current  2025-05-05 22:56:15  25.05.20250423.8a2f738  6.14.3-zen1                          *

--- modules/drivers/nvidia-drivers.nix
+++ modules/drivers/nvidia-drivers.nix
@@ -19,6 +19,15 @@ in {
       enable32Bit = true;
     };
 
+    nix.settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     services.xserver.videoDrivers = ["nvidia"];
 
     boot.kernelParams = [
