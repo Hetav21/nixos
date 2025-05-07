ca17effa28ec53baf5dcadc85ad0d34383490dc4
Hetav21 <shahhetav2106@gmail.com>
2025-05-07 12:58:32 +0530

    133 current  2025-05-07 12:58:26  25.05.20250423.8a2f738  6.14.3-zen1                          *

--- hosts/nixbook/user.nix
+++ hosts/nixbook/user.nix
@@ -10,13 +10,10 @@ in {
   options = {
   };
   config = {
-    programs.fish.enable = true;
-
     users.users.${userName} = {
       isNormalUser = true;
       description = userDescription;
-      shell = pkgs.fish;
-      # shell = pkgs.nushell;
+      shell = pkgs.nushell;
       extraGroups = ["wheel" "mlocate" "docker" "wireshark"];
     };
   };
