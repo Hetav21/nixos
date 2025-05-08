07983388ab7085a80b2bd54e3c56d7c71059585b
Hetav21 <shahhetav2106@gmail.com>
2025-05-08 21:56:01 +0530

    142 current  2025-05-08 21:55:57  25.05.20250423.8a2f738  6.14.3-zen1                          *

--- hosts/nixbook/home.nix
+++ hosts/nixbook/home.nix
@@ -137,11 +137,11 @@ in {
           timeout = 900;
           on-timeout = "hyprlock";
         }
-        # {
-        #   timeout = 1200;
-        #   on-timeout = "hyprctl dispatch dpms off";
-        #   on-resume = "hyprctl dispatch dpms on";
-        # }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
       ];
     };
   };
