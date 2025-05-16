66354ac7e57102ce4c5d2dbde07920d9744124be
Hetav21 <shahhetav2106@gmail.com>
2025-05-16 16:57:37 +0530

    175 current  2025-05-16 16:57:31  25.05.20250513.adaa24f  6.14.6-cachyos                          *

--- system/units/_fonts.nix
+++ system/units/_fonts.nix
@@ -22,5 +22,27 @@ in {
     mplus-outline-fonts.githubRelease
     font-awesome
     material-icons
+    nerd-fonts.ubuntu-sans
   ];
+
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerd-fonts.ubuntu-sans;
+      name = "Ubuntu Mono";
+    };
+    sansSerif = {
+      package = pkgs.nerd-fonts.ubuntu-sans;
+      name = "Ubuntu";
+    };
+    serif = {
+      package = pkgs.nerd-fonts.ubuntu-sans;
+      name = "Ubuntu";
+    };
+    sizes = {
+      applications = 12;
+      terminal = 15;
+      desktop = 11;
+      popups = 12;
+    };
+  };
 }
--- system/units/desktop.nix
+++ system/units/desktop.nix
@@ -61,26 +61,6 @@ in {
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
     cursor.size = 24;
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font Mono";
-      };
-      sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      sizes = {
-        applications = 12;
-        terminal = 15;
-        desktop = 11;
-        popups = 12;
-      };
-    };
   };
 
   xdg = {
