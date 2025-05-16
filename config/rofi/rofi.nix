b9a1cae47288558db10531b89fba10f5728287b0
Hetav21 <shahhetav2106@gmail.com>
2025-05-16 17:59:15 +0530

    ..

--- config/rofi/rofi.nix
+++ config/rofi/rofi.nix
@@ -13,7 +13,7 @@
         show-icons = true;
         icon-theme = "Papirus";
         location = 0;
-        # font = "JetBrainsMono Nerd Font Mono 12";
+        font = "JetBrainsMono Nerd Font Mono 12";
         drun-display-format = "{icon} {name}";
         display-drun = " Apps";
         display-run = " Run";
