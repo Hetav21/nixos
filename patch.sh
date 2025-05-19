469fc2a571adb7306e06f48b8cd057aeff38e94f
Hetav21 <shahhetav2106@gmail.com>
2025-05-19 16:05:38 +0530

    add better patch script

--- patch.sh
+++ patch.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-git apply patch/* --allow-empty
-echo "Patch Applied"
+if [[ -d "patch" && "$(ls -A patch)" ]]; then
+    git apply patch/* --allow-empty
+    echo "Patch Applied"
+
+    read -p "Do you want to delete all patches in the 'patch' directory? (y/N): " confirmation
+
+    if [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
+        rm -rf ./patch/*
+        echo "All patches in 'patch' have been deleted."
+    else
+        echo "No patches were deleted."
+    fi
+else
+    echo "The 'patch' directory is empty or it does not exists"
+fi
