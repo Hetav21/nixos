84f225743dfbd3419ca608292711c2950eda5a4a
Hetav21 <shahhetav2106@gmail.com>
2025-07-27 14:11:33 +0530

    Adding more configs to zen browser

--- config/zen-browser.nix
+++ config/zen-browser.nix
@@ -12,17 +12,70 @@
   ];
 
   zen-nebula = {
-    enable = true;
+    enable = false; # set to true if you want to use nebula theme from pkgs
     profile = settings.username;
+  };
+
+  stylix.targets.zen-browser = {
+    enable = true; # set to true if you want to use nebula theme from Stylix
+    profileNames = ["${settings.username}"];
   };
 
   programs.zen-browser = {
     enable = true;
     nativeMessagingHosts = [pkgs.firefoxpwa];
+    profiles = {
+      "${settings.username}" = {
+        isDefault = true;
+      };
+    };
     policies = {
-      DisableAppUpdate = true;
-      DisableTelemetry = true;
       # find more options here: https://mozilla.github.io/policy-templates/
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      Preferences = {
+        "browser.tabs.warnOnClose" = {
+          "Value" = false;
+          "Status" = "locked";
+        };
+        "zen.view.grey-out-inactive-windows" = {
+          "Value" = false;
+          "Status" = "locked";
+        };
+      };
+      SearchEngines = {
+        Default = "Unduck";
+        Add = [
+          {
+            Name = "Unduck";
+            URL = "https://unduck.link?q={searchTerms}";
+            Alias = "unduck";
+          }
+          {
+            Name = "NixOS Wiki";
+            URL = "https://nixos.wiki/index.php?search={searchTerms}";
+            Icon = "https://nixos.wiki/favicon.ico";
+            Alias = "nw";
+          }
+        ];
+        Remove = [
+          "Bing"
+        ];
+      };
     };
   };
 }
