b5428e65c04656e7fda54d23ae52f301ee77cd29
Hetav21 <shahhetav2106@gmail.com>
2025-12-23 06:46:50 +0000

    Removing unused templates and updating the existing ones

--- templates/uv/flake.nix
+++ templates/uv/flake.nix
@@ -1,45 +1,71 @@
 {
   description = "A Nix-flake-based Python development environment with uv package manager";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          uv
-          python312
-          nodejs
-          stdenv.cc.cc.lib
-        ];
+  outputs =
+    { self, ... }@inputs:
 
-        shellHook = ''
-          echo "🐍 Python development environment with uv/uvx"
-          echo "uv version: $(uv --version)"
-          echo "uvx is available via: uv tool run (or uvx alias)"
-          echo ""
-        '';
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
 
-        # Make native libraries available to Python packages
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc.lib
-        ];
-      };
-    });
+      # Change this value ({major}.{minor}) to update the Python version
+      version = "3.13";
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        let
+          concatMajorMinor =
+            v:
+            pkgs.lib.pipe v [
+              pkgs.lib.versions.splitVersion
+              (pkgs.lib.sublist 0 2)
+              pkgs.lib.concatStrings
+            ];
 
-    # Also expose uv as a package
-    packages = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.uv;
-    });
-  };
+          python = pkgs."python${concatMajorMinor version}";
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.uv
+              python
+              pkgs.nodejs
+            ];
+
+            shellHook = ''
+              echo "🐍 Python ${version} development environment with uv/uvx"
+              echo "uv version: $(uv --version)"
+              echo ""
+            '';
+
+            # Make native libraries available to Python packages
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc.lib
+            ];
+          };
+        }
+      );
+
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.uv;
+        }
+      );
+    };
 }
