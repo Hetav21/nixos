e63705d9316da2b81e1d7c41d0acc209d3af8f78
Hetav21 <shahhetav2106@gmail.com>
2026-08-29 16:51:55 +0530

    refactor(home): remove legacy compatibility shims from home.system category module

--- src/modules/home/system/default.nix
+++ src/modules/home/system/default.nix
@@ -1,6 +1,5 @@
 {
   extraLib,
-  lib,
   ...
 } @ args:
 extraLib.modules.mkCategoryModule args {
@@ -17,30 +16,4 @@ extraLib.modules.mkCategoryModule args {
     "downloads"
     "settings"
   ];
-
-  extraOptions = {
-    # Backwards compatibility options
-    home.downloads.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable downloads utilities";
-    };
-
-    home.nixSettings.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable Nix development settings";
-    };
-
-    home.system.nix.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Alias for home.system.settings.enable";
-    };
-  };
-
-  extraConfig = {config, ...}: {
-    home.system.downloads.enable = lib.mkDefault config.home.downloads.enable;
-    home.system.settings.enable = lib.mkDefault (config.home.nixSettings.enable || config.home.system.nix.enable);
-  };
 }
